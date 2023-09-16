3efdc9db2131267d764fcae441572608c9abba04
mvase <mvase@LAPTOP-ELU0GS4U>
2023-09-16 19:09:06 +0700

    Добавьте файлы проекта.

---
+++ Functions.cs
@@ -0,0 +1,101 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+using System.Collections.ObjectModel;
+
+namespace Lab1
+{
+    public class Functions
+    {
+        public static ObservableCollection<Student> ReadAllDate(string path)
+        {
+            ObservableCollection<Student> students = new ObservableCollection<Student>();
+            try
+            {
+                using (StreamReader sr = new StreamReader(path))
+                {
+                    string tempNotes;
+                    while ((tempNotes = sr.ReadLine()) != null)
+                    {
+                        students.Add(new Student(tempNotes.Split(',')[0], tempNotes.Split(',')[1],
+                                                tempNotes.Split(',')[2], Convert.ToBoolean(tempNotes.Split(',')[3]),
+                                                Convert.ToInt32(tempNotes.Split(',')[4])));
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+
+            return students;
+        }
+
+        public static void PrintAllNotes(ObservableCollection<Student> students)
+        {
+            for (int i = 0; i < students.Count; i++)
+            {
+                Console.WriteLine((i + 1) + ": " + "ФИО: " + students[i].Surname + " " + students[i].Name + " " + students[i].Patronymic +
+                    " | Пол: " + ConvertSex(students[i].Sex) + " | Возраст: " + students[i].Age
+                    );
+            }
+            Console.WriteLine("\n");
+        }
+
+        public static string ConvertSex(bool s)
+        {
+            if (s == true) { return "М"; }
+            else { return "Ж"; }
+        }
+
+        public static void PrintNotesByNumber(int note_number, ref ObservableCollection<Student> students)
+        {
+            try
+            {
+                Student nstudent = students[note_number - 1];
+                Console.WriteLine("ФИО: " + nstudent.Surname + " "
+                                + nstudent.Name + " "
+                                + nstudent.Patronymic + " | "
+                                + "Пол: " + ConvertSex(nstudent.Sex) + " | "
+                                + "Возраст: " + nstudent.Age + "\n");
+            }
+            catch (Exception e) { Console.WriteLine("Такой записи нет!\n"); }
+
+        }
+
+        public static void WriteNotesToFile(ref ObservableCollection<Student> students, string path)
+        {
+            using (StreamWriter sw = new StreamWriter(path, false))
+            {
+                for (int i = 0; i < students.Count; i++)
+                {
+                    string student_line = students[i].Surname + "," + students[i].Name + "," +
+                        students[i].Patronymic + "," + students[i].Sex + "," + students[i].Age;
+                    sw.WriteLine(student_line);
+                }
+            }
+            Console.WriteLine("Даныне записаны в файл!\n");
+        }
+
+        public static void RemoveNotesFromFile(int note_number, ref ObservableCollection<Student> students)
+        {
+            try { students.RemoveAt(note_number); }
+            catch (Exception e) { Console.WriteLine("Записи с таким номром не существует!\n"); }
+        }
+
+        public static void AddNoteToFile(string surname, string name, string patronymic, string sex, int age, ref ObservableCollection<Student> students)
+        {
+            bool tsex = true;
+            if (sex == "М" || sex == "м") { tsex = true; }
+            if (sex == "Ж" || sex == "ж") { tsex = false; } else { Console.WriteLine("Форма заполнена не верно!\n"); }
+            try
+            {
+                students.Add(new Student(surname, name, patronymic, tsex, age));
+            }
+            catch(Exception e) { Console.WriteLine("Форма заполнена не верно!\n"); }
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Linq;
+
+namespace Lab1
+{
+    class Program
+    {
+        static string path = "students.csv";
+        static void Main(string[] args)
+        {
+            ObservableCollection<Student> students = Functions.ReadAllDate(path);
+            while (true)
+            {
+                ConsoleKey ans;
+                int note_number;
+                Console.WriteLine("Что хотите сделать?:\n" +
+                    "1. Вывод всех записей на экран.\n" +
+                    "2. Вывод записи по номеру.\n" +
+                    "3. Запись данных в файл.\n" +
+                    "4. Удаление записи из файла.\n" +
+                    "5. Добавление записи в файл.\n" +
+                    "Выход из приложения - ESC.\n");
+                ans = Console.ReadKey().Key;
+                Console.WriteLine();
+
+                switch (ans)
+                {
+                    case ConsoleKey.D1:
+                        Functions.PrintAllNotes(students);
+                        break;
+                    case ConsoleKey.D2:
+                        Console.WriteLine("Введите номер записи для вывода: ");
+                        note_number = Convert.ToInt32(Console.ReadLine());
+                        Functions.PrintNotesByNumber(note_number, ref students);
+                        break;
+                    case ConsoleKey.D3:
+                        Functions.WriteNotesToFile(ref students, path);
+                        break;
+                    case ConsoleKey.D4:
+                        Console.WriteLine("Введите номер записи для удаления: ");
+                        int note_delete = Int32.Parse(Console.ReadLine()) - 1;
+                        Functions.RemoveNotesFromFile(note_delete, ref students);
+                        break;
+                    case ConsoleKey.D5:
+                        Console.WriteLine("Введите фамилию: ");
+                        string surname = Console.ReadLine().ToString();
+                        Console.WriteLine("Введите имя: ");
+                        string name = Console.ReadLine().ToString();
+                        Console.WriteLine("Введите отчество: ");
+                        string patronomyc = Console.ReadLine().ToString();
+                        Console.WriteLine("Введите пол (м/ж): ");
+                        string sex = Console.ReadLine();
+                        Console.WriteLine("Введите возраст: ");
+                        int age = Int32.Parse(Console.ReadLine());
+                        Functions.AddNoteToFile(surname, name, patronomyc, sex, age, ref students);
+                        break;
+                    case ConsoleKey.Escape:
+                        return;
+                    default:
+                        Console.WriteLine("Такого пункта меню нет!\n");
+                        break;
+                }
+            }
+        }
+    }
+}
